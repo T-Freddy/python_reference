a70d28108f1f13edcacca15d57fe292e300bc5a9
Freddy Sehlapelo <35729472+T-Freddy@users.noreply.github.com>
2019-10-22 14:17:26 +0200

    Update images.py

--- images.py
+++ images.py
@@ -1,16 +1,19 @@
 # import packages
 import cv2 as cv
 import pytesseract
+from scipy import ndimage
 
 # convrting image to numpy array
 new_image = np.asarray(image)
 
 # plotting image in grayscale
 plt.imshow(image, cmap='gray')
-
 # applying adaptive thresholding with CV2
 new_image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, 1)
 new_image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, 1)
+
+# denoising
+new_image = ndimage.median_filter(image, 3)
 
 # image to text
 text = pytesseract.image_to_string(image, lang='eng')
