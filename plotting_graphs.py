ed5475f8c8b58f6c2926ab63833e9f5ba18fdbe6
Freddy Sehlapelo <35729472+T-Freddy@users.noreply.github.com>
2019-10-22 12:01:44 +0200

    Create plotting_graphs.py

---
+++ plotting_graphs.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ploting multiple graphs
+plt.subplot(1,2,1)
+plt.plot() # graph 1
+plt.subplot(1,2,2)
+plt.plot() # graph 2
+
+# increasing dots per inch (dpi)
+plt.figure(dpi = 1600)
+
+# ploting an image
+plt.imshow(image)
