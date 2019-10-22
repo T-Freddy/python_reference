860c93467fd587e254ea48660cc2cee4249e542c
Freddy Sehlapelo <35729472+T-Freddy@users.noreply.github.com>
2019-10-22 12:19:54 +0200

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,1 +1,11 @@
 import pandas as pd
+
+# filter df based on column value
+df[df['col'] == num]
+df[df['col'].str.contains(string)]
+
+df[(df['col 1'] == num_1) & (df['col 2'] == num_2)] # add operation
+df[(df['col 1'] == num_1) | (df['col 2'] == num_2)] # or operation
+
+# negate a boolean filter
+df[~(df['col'] == num)]
